d0800af4a449f8a5345887eaef1fac2e204d5ba3
nimra <nimra271430@gmail.com>
2024-03-31 11:28:29 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// Addition 
+var num1 = 2;
+var num2 = 4;
+var total = 2 + 4;
+console.log(total);
+// subtraction
+var num3 = 3;
+var num4 = 5;
+var subtraction = 5 - 3;
+console.log(subtraction);
+//multiplication
+var num5 = 10;
+var num6 = 2;
+var multiplication = 10 * 2;
+console.log(multiplication);
+//division
+var num7 = 10;
+var num8 = 2;
+var devision = 2 / 10;
+console.log(devision);
---
+++ main.ts
@@ -0,0 +1,25 @@
+
+// Addition 
+let num1: number = 2
+let num2: number = 4
+let total: number = 2 + 4;
+console.log(total);
+
+// subtraction
+
+let num3: number = 3
+let num4: number = 5
+let subtraction: number = 5 - 3;
+console.log(subtraction)  
+
+//multiplication
+let num5: number = 10
+let num6: number = 2
+let multiplication: number = 10 * 2;
+console.log(multiplication);
+
+//division
+let num7: number = 10 
+let num8: number = 2
+let devision: number = 2 / 10;
+console.log(devision);  
